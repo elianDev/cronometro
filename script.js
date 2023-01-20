6535b4afaf79b41fa31e298040a24c1b4c0cd357
Elian Silva <you@example.com>
2023-01-20 16:12:14 -0300

    formatado timer

--- script.js
+++ script.js
@@ -16,18 +16,8 @@ time.innerText = "00:00:00";
 
 function initCronometer() {
   init.setAttribute("disabled", "");
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
   timer = setInterval(() => {
     seconds++;
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-
     if (seconds === 60) {
       minutes++;
       seconds = 0;
@@ -36,7 +26,9 @@ function initCronometer() {
       hours++;
       minutes = 0;
     }
-    time.innerText = `${hours}:${minutes}:${seconds}`;
+    time.innerText = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(
+      seconds
+    )}`;
   }, 1000);
 }
 
@@ -53,7 +45,5 @@ function resetCronometer() {
 }
 
 function formatTime(n) {
-  if (n < 10) {
-    n = `0${n}`;
-  }
+  return n >= 10 ? n : `0${n}`;
 }
